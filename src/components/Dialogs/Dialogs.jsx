0ee509d4089d0fcfd22f8afb511fb128ac5b06c4
Валерий Попов <bormancmexv@gmail.com>
2020-03-28 19:15:12 +0300

    added route in app

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,11 @@ import s from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div>
-            Dialogs
+            Dialogs ddddded
+            egergwee
+            ergewgre
+            gewrgewrg
+            ergewrgweg
         </div>
     )
 
