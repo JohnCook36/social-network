5cfda30ba46b747d2e348b266fbaceed50805b3f
Валерий Попов <bormancmexv@gmail.com>
2020-03-30 20:06:27 +0300

    added .map to MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,11 +4,15 @@ import Posts from './Post/Posts';
 
 const MyPosts = (props) => {
 
-    let postData = [
+    let posts = [
         {id: 1, message: 'Hi, how are you', likeCount:12 },
-        {id: 2, message: 'It is good day', likeCount: 10}
+        {id: 2, message: 'It is good day', likeCount: 10},
+        {id: 2, message: ' good ', likeCount: 10},
+        {id: 2, message: 'Hi', likeCount: 10}
     ]
 
+    let postsElements =
+        posts.map( p => <Posts message={p.message} likesCount={p.likeCount}/>)
 
     return (
         <div className={s.postsBlock}>
@@ -23,8 +27,7 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Posts message={postData[0].message} likesCount={postData[0].likeCount}/>
-                <Posts message={postData[1].message} likesCount={postData[1].likeCount}/>
+                { postsElements }
             </div>
         </div>
     )
