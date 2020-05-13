be412184aa3e434103abc8fcc56bdef3c10a6bd9
Aldushin Ivan <ivan.aldushin@alphaott.com>
2020-05-13 21:37:16 +0300

    Correct pagination view

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,19 +5,23 @@ import {NavLink} from "react-router-dom";
 
 let Users = (props) => {
 
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize) / 50;
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
     let pages = []
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
 
+    if (pages.length > 20){
+        pages = [...pages.slice(0, 10), '...', ...pages.slice(pages.length - 10, pages.length)]
+    }
+
     return <div>
         <div>
             {pages.map(p =>
                 <span
-                    className={props.currentPage === p && s.selectedPage || ''}
-                    onClick={(e) => props.onPageChanged(p)}
+                    className={props.currentPage === p ? s.activePagePointer : s.pagePointer }
+                    onClick={(e) => p !=='...' && props.onPageChanged(p)}
                     key={p}
                 >
                     {p}
